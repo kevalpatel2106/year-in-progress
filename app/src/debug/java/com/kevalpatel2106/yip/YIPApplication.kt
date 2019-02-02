58d802a08d037b8f4f7a74acabfb60e3054cd8e7
Keval Patel <kevalpatel2106@gmail.com>
2019-02-02 17:49:41 +0000

    Enable firebase prefs plugin
    Signed-off-by: Keval Patel <kevalpatel2106@gmail.com>

--- app/src/debug/java/com/kevalpatel2106/yip/YIPApplication.kt
+++ app/src/debug/java/com/kevalpatel2106/yip/YIPApplication.kt
@@ -4,17 +4,18 @@ import android.app.Application
 import com.facebook.stetho.Stetho
 import com.google.android.gms.ads.MobileAds
 import com.kevalpatel2106.yip.core.di.CoreComponent
+import timber.log.Timber
 
 internal class YIPApplication : Application() {
     internal val coreComponent by lazy { CoreComponent.build(this@YIPApplication) }
 
     override fun onCreate() {
         super.onCreate()
-
         // Initialize admob
         MobileAds.initialize(this, getString(R.string.admob_app_id))
 
-        // Enable stetho
+        // Debugging
+        Timber.plant(Timber.DebugTree())
         Stetho.initializeWithDefaults(this)
     }
 }
