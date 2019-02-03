97e07d432cb8aeec721b664fe4502aca961d1243
Keval Patel <kevalpatel2106@gmail.com>
2019-02-03 09:27:30 +0000

    Created center title preference for "Made in UK badge"
    Signed-off-by: Keval Patel <kevalpatel2106@gmail.com>

---
+++ app/src/main/java/com/kevalpatel2106/yip/settings/CentralTitlePreference.kt
@@ -0,0 +1,21 @@
+package com.kevalpatel2106.yip.settings
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
+
+internal class CentralTitlePreference @JvmOverloads constructor(
+        context: Context,
+        attributes: AttributeSet? = null,
+        diffStyleAttributes: Int = 0
+) : Preference(context, attributes, diffStyleAttributes){
+
+    override fun onBindViewHolder(holder: PreferenceViewHolder?) {
+        super.onBindViewHolder(holder)
+        (holder?.findViewById(android.R.id.title) as? AppCompatTextView)?.gravity = Gravity.CENTER
+        (holder?.findViewById(android.R.id.summary) as? AppCompatTextView)?.gravity = Gravity.CENTER
+    }
+}
