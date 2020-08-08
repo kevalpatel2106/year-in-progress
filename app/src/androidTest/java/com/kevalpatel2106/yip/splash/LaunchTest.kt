9404d2311a3ac50da5804ea065f554a5f5ca54d3
Keval Patel <kevalpatel2106@gmail.com>
2020-08-08 12:25:19 +0100

    Annotated launch test with large test

--- app/src/androidTest/java/com/kevalpatel2106/yip/splash/LaunchTest.kt
+++ app/src/androidTest/java/com/kevalpatel2106/yip/splash/LaunchTest.kt
@@ -5,7 +5,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.MediumTest
+import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.kevalpatel2106.yip.R
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -14,7 +14,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@MediumTest
+@LargeTest
 @HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 class LaunchTest {
